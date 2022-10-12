68c60a086d3d6d8d8ea31f70f2addd54c13a0fe8
sadock123 <200020038@iitdh.ac.in>
2022-10-12 14:36:37 +0530

    added portfolio

---
+++ src/component/about.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './about.css';
+function About() {
+  return (
+    <div className='about'>
+     <div className='about1'> About Section</div>
+      <br/>
+      <br/>
+      <br/>
+     <div className='about_content'>
+     Hi there, I am Sadock Chakma from Guwahati, India.
+I am currently in the 3rd year of my BTech at IIT Dharwad. 
+<br/> I am currently working on Frontend Development.
+My hobbies are gaming, watching anime, daydreaming and reading tech related artticles.
+     </div>
+      </div>
+  )
+}
+
+export default About
