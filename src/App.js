820fdc70b2b8e7de8fbd870a74c60b90e8f599d7
sadock123 <200020038@iitdh.ac.in>
2022-10-15 23:13:02 +0530

    updated the div and used grid

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Main from './component/main.js';
-import ButtonAppBar from './component/navbar';
 import About from './component/about.js';
 import Experiences from './component/experiences.js'
 function App() {
   return (
     <div>
- 
+ <br/>
+ <br/>
   <Main />
   <About />
   <Experiences/>
--- src/component/main.js
+++ src/component/main.js
@@ -1,32 +1,26 @@
 /* eslint-disable jsx-a11y/img-redundant-alt */
 import React from 'react'
 import './main.css';
-import Image from './image/Sadock.jpeg'
 import Avatar from '@mui/material/Avatar';
+import { Box,Grid, Typography } from '@mui/material';
+import HomeSVG from './HomeSVG.js';
+
 function main() {
   return (
-    <div className='root'>
-        <div className="heading"><h3>Welcome to Sadock's Portfolio</h3></div>
-        <div className="contents">
-        <div className="writing">
-         Hey, My name is Sadock Chakma and I am at my pre-final year of his college Life in Indian Institue of Technology, Dharwad.
-          <br/>My major is in Electrical Engineering, but my hobby is in Coding .
-          <br/>
-          <br/>
-          <br/>
-          I use Cpp, Python, Javascript for my programmes.
-            </div>
-        <div className='image'>
-          <div className="myphoto">
-          <Avatar
-        alt="Sadock Chakma"
-        src={Image}
-        sx={{ width: '15vw', height: '40vh' }}
-      />
-          </div>
-        </div>
-          </div>
-    </div>
+<Grid container sx={{ flexDirection: { xs: 'column-reverse', md: 'row' } }}>
+<Grid item md={6} xs={12}>
+  <br/>
+  <br/>
+        <Typography variant="h4">
+        
+         Hey, My name is Sadock Chakma.
+        </Typography>
+        </Grid>
+        <Grid item md={6} xs={12}>  
+        <HomeSVG/>  
+      </Grid>
+    </Grid>
+
   )
 }
 
