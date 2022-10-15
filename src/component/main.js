6728e1c17086206544a3fc63fe392722e5f163d9
sadock123 <200020038@iitdh.ac.in>
2022-10-16 03:07:07 +0530

    Added extra data along with the design of the about page

--- src/component/main.js
+++ src/component/main.js
@@ -2,23 +2,27 @@
 import React from 'react'
 import './main.css';
 import Avatar from '@mui/material/Avatar';
+
 import { Box,Grid, Typography } from '@mui/material';
 import HomeSVG from './HomeSVG.js';
 
 function main() {
   return (
-<Grid container sx={{ flexDirection: { xs: 'column-reverse', md: 'row' } }}>
-<Grid item md={6} xs={12}>
+<Grid container sx={{ flexDirection: { xs: 'column-reverse', md: 'row',alignItem:"center", justifyContent:"center" } }}>
+<Grid item xs={12} md={6} sx={{ textAlign: 'justify' }}>
   <br/>
   <br/>
-        <Typography variant="h4">
+        <Typography variant="h6">
         
-         Hey, My name is Sadock Chakma.
+         Hey, My name is
         </Typography>
+        <Typography variant="h3">
+           Sadock Chakma.
+       </Typography>
         </Grid>
-        <Grid item md={6} xs={12}>  
-        <HomeSVG/>  
-      </Grid>
+                <Grid item md={2} sx={{ display: { xs: 'none', md: 'flex' }, alignItems: 'center', justifyContent: 'center' }}>
+                    <HomeSVG />
+                </Grid>
     </Grid>
 
   )
